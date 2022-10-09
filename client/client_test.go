3bf012a545b7fe848c7cca42f895f0516e7e4aa5
vishruthsk <vishruthsk@gmail.com>
2022-10-09 14:17:50 +0530

    init

---
+++ client/client_test.go
@@ -0,0 +1,129 @@
+package client
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/require"
+	"github.com/vishruthsk/utils-go/mock-client"
+)
+
+func TestNewDefaultClient(t *testing.T) {
+	c := require.New(t)
+
+	client := NewDefaultClient()
+	c.NotEmpty(client)
+
+	client = NewCustomClient(5, 3*time.Second)
+	c.NotEmpty(client)
+}
+
+func TestClient_PostWithURLJSONParams(t *testing.T) {
+	c := require.New(t)
+
+	client := NewDefaultClient()
+	c.NotEmpty(client)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock.AddMockedResponseFromFile(http.MethodPost, "https://dummy.com", http.StatusCreated, "../mock-client/samples/dummy.json")
+
+	response, err := client.PostWithURLJSONParams("https://dummy.com", map[string]string{
+		"ohana": "family",
+	}, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusCreated, response.StatusCode)
+	c.NoError(response.Body.Close())
+
+	response, err = client.PostWithURLJSONParams("https://dummy.com", nil, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusCreated, response.StatusCode)
+	c.NoError(response.Body.Close())
+}
+
+func TestClient_PutWithURLJSONParams(t *testing.T) {
+	c := require.New(t)
+
+	client := NewDefaultClient()
+	c.NotEmpty(client)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock.AddMockedResponseFromFile(http.MethodPut, "https://dummy.com", http.StatusCreated, "../mock-client/samples/dummy.json")
+
+	response, err := client.PutWithURLJSONParams("https://dummy.com", map[string]string{
+		"ohana": "family",
+	}, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusCreated, response.StatusCode)
+	c.NoError(response.Body.Close())
+
+	response, err = client.PutWithURLJSONParams("https://dummy.com", nil, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusCreated, response.StatusCode)
+	c.NoError(response.Body.Close())
+}
+
+func TestClient_PostWithURLEncodedParams(t *testing.T) {
+	c := require.New(t)
+
+	client := NewDefaultClient()
+	c.NotEmpty(client)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock.AddMockedResponseFromFile(http.MethodPost, "https://dummy.com", http.StatusCreated, "../mock-client/samples/dummy.json")
+
+	params := url.Values{}
+	params.Add("ohana", "family")
+
+	response, err := client.PostWithURLEncodedParams("https://dummy.com", params, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusCreated, response.StatusCode)
+	c.NoError(response.Body.Close())
+
+	response, err = client.PostWithURLJSONParams("https://dummy.com", nil, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusCreated, response.StatusCode)
+	c.NoError(response.Body.Close())
+}
+
+func TestClient_GetWithURLAndParams(t *testing.T) {
+	c := require.New(t)
+
+	client := NewDefaultClient()
+	c.NotEmpty(client)
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	mock.AddMockedResponseFromFile(http.MethodGet, "https://dummy.com", http.StatusOK, "../mock-client/samples/dummy.json")
+
+	params := url.Values{}
+	params.Set("family", "ohana")
+
+	response, err := client.GetWithURLAndParams("https://dummy.com", params, http.Header{})
+	c.NoError(err)
+
+	c.NotEmpty(response)
+	c.Equal(http.StatusOK, response.StatusCode)
+	c.NoError(response.Body.Close())
+}
---
+++ environment/environment.go
@@ -0,0 +1,121 @@
+// Package environment is a set of functions to get env values or their default
+// It has the autoload from .env files
+package environment
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	// autoload env vars
+	_ "github.com/joho/godotenv/autoload"
+)
+
+// MustGetInt64 gets the required env var as an int and panics if it is not present
+func MustGetInt64(varName string) int64 {
+	val, ok := os.LookupEnv(varName)
+	if !ok {
+		panic(fmt.Sprintf("environment error (int64): required env var %s not found", varName))
+	}
+
+	iVal, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("environment error (int64): unable to parse value %s to required env var %s", val, varName))
+	}
+
+	return iVal
+}
+
+// GetInt64 gets the env var as an int
+func GetInt64(varName string, defaultValue int64) int64 {
+	val, ok := os.LookupEnv(varName)
+	if !ok {
+		return defaultValue
+	}
+
+	iVal, err := strconv.ParseInt(val, 10, 64)
+	if err != nil {
+		return defaultValue
+	}
+
+	return iVal
+}
+
+// MustGetString gets the required environment var as a string and panics if it is not present
+func MustGetString(varName string) string {
+	val, _ := os.LookupEnv(varName)
+	if val == "" {
+		panic(fmt.Sprintf("environment error (string): required env var %s not found", varName))
+	}
+
+	return val
+}
+
+// GetString gets the environment var as a string
+func GetString(varName string, defaultValue string) string {
+	val, _ := os.LookupEnv(varName)
+	if val == "" {
+		return defaultValue
+	}
+
+	return val
+}
+
+// MustGetBool gets the required environment var as a bool and panics if it is not present
+func MustGetBool(varName string) bool {
+	val, _ := os.LookupEnv(varName)
+	if val == "" {
+		panic(fmt.Sprintf("environment error (bool): required env var %s not found", varName))
+	}
+
+	iVal, err := strconv.ParseBool(val)
+	if err != nil {
+		panic(fmt.Sprintf("environment error (bool): unable to parse value %s to required env var %s", val, varName))
+	}
+
+	return iVal
+}
+
+// GetBool gets the environment var as a bool
+func GetBool(varName string, defaultValue bool) bool {
+	val, _ := os.LookupEnv(varName)
+	if val == "" {
+		return defaultValue
+	}
+
+	iVal, err := strconv.ParseBool(val)
+	if err != nil {
+		return defaultValue
+	}
+
+	return iVal
+}
+
+// MustGetStringMap gets the required environment var as a string map and panics if it is not present
+func MustGetStringMap(varName, separator string) map[string]bool {
+	rawString := MustGetString(varName)
+	stringSlice := strings.Split(rawString, separator)
+
+	stringMap := make(map[string]bool, len(stringSlice))
+
+	for _, singleString := range stringSlice {
+		stringMap[singleString] = true
+	}
+
+	return stringMap
+}
+
+// GetStringMap gets the environment var as a string map
+func GetStringMap(varName, defaultValue, separator string) map[string]bool {
+	rawString := GetString(varName, defaultValue)
+	stringSlice := strings.Split(rawString, separator)
+
+	stringMap := make(map[string]bool, len(stringSlice))
+
+	for _, singleString := range stringSlice {
+		stringMap[singleString] = true
+	}
+
+	return stringMap
+}
